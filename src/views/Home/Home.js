ab2ecfd645a5f38abdc415d0496fdaf3ff4c6253
kaoutarBa <baldi.kaoutar.ma@gmail.com>
2020-09-10 15:55:33 +0100

    style:Home

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import question from 'assets/question.svg';
-import {Img,H1} from './Home.styles';
+import {Img,P,Title,Container,StartBtn,Section} from './Home.styles';
+
 const Home=()=>{
     return(
-        <div>
-            <Img src={question} alt={"question"}/>
-          <H1>Quiz App</H1> 
-        </div>
+    <Container>
+        <Img src={question} alt={"question"}/>
+        <Section>
+            <Title>Quiz App</Title>
+            <P>  Test Your Knowledge!</P>
+        </Section>
+        <StartBtn type={"button"}>Start</StartBtn>
+    </Container>
     );
 
 };
--- src/views/Home/Home.styles.js
+++ src/views/Home/Home.styles.js
@@ -1,14 +1,58 @@
 import styled from 'styled-components';
 
+
+const Container=styled.div`
+height:95vh;
+width:100%;
+display:grid;
+grid-template-columns:50% 5% 35% 10%;
+grid-template-rows:15% 45% 25% 15%;
+`;
 const Img=styled.img`
-height:50%;
-width:50%;
+height:400px;
+grid-column:1/2;
+grid-row:2/4;
 `;
 
-const H1=styled.h1`
-
+const Title=styled.div`
+font-size:50px;
+font-weight:500;
 color:#00BFA6;
+`;
+const P=styled.div`
+padding-left:10px;
+padding-top:5px;
+`;
+
+const Section=styled.section`
+grid-column:2/4;
+grid-row:2/3;
+align-self:center;
+justify-self:start;
 
 `;
 
-export {Img,H1};
+const StartBtn=styled.button`
+align-self:start;
+justify-self:center;
+grid-column:3/4;
+grid-row:3/4;
+border:2px solid  #00BFA6;
+height:40px;
+width:170px;
+color:#00BFA6;
+border-radius:11px;
+font-size:23px;
+font-weight:500;
+cursor:pointer;
+outline:none;
+background:white;
+&:hover{
+    background:#00BFA6;
+    color:white;
+    border:none;
+}
+`;
+
+
+export {Img,P,Title,Container,StartBtn,Section};
