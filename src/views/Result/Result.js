13c182db215b8e22549a18c2022df71b3fe070a8
kaoutarBa <baldi.kaoutar.ma@gmail.com>
2020-09-12 02:42:02 +0100

    styling: result

--- src/views/Result/Result.js
+++ src/views/Result/Result.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import {ResultSection,YouGot,Button,Container,YourResult,A,ButtonsSection} from './Result.styles';
 
 const Result=({result})=>{
     return(
-    <div>
-        You Got {Math.floor((result/15)*100)}% !
-    </div>
+    <Container>
+     <YourResult>Your Result</YourResult>   
+    <ResultSection>
+       <YouGot>You Got {Math.floor((result/15)*100)}% !
+       </YouGot> 
+    </ResultSection>
+    <ButtonsSection>
+    <Button type="button" >
+    <A href={"/"}> Home</A>
+    </Button>
+    <Button type="button"><A href={"/quiz"}> Redo Quiz</A></Button>
+    <Button type="button">
+        <A href={"javascript:alert('quizapp-ba.com ');"}>Share</A>
+    </Button>
+    </ButtonsSection>
+   
+    </Container>    
+    
     );
 
 };
---
+++ src/views/Result/Result.styles.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import resultBg from 'assets/resultBg.svg';
+
+
+const Container=styled.div`
+height:70vh;
+width:100%;
+background-image: url(${resultBg});
+background-repeat: no-repeat;
+display:grid;
+grid-template-rows:15% 75%;
+grid-template-columns:15% 70% 15%;
+`;
+const ResultSection=styled.div`
+width: 546px;
+height: 292px;
+background: rgba(0, 191, 166, 0.17);
+border-radius: 10px;
+display:flex;
+grid-column:2/3;
+grid-row:2/3;
+justify-self:center;
+align-self:center;
+`;
+const YouGot=styled.div`
+margin:auto;
+font-size: 30px;
+font-weight:400;
+color: #095066;
+`;
+
+const Button=styled.button`
+width: 88px;
+height: 36px;
+border:none;
+background: #3BA6C8;
+border-radius: 5px;
+outline:none;
+cursor: pointer;
+`;
+const YourResult=styled.div`
+width: 232px;
+height: 53px;
+font-style: normal;
+font-weight: normal;
+font-size: 20px;
+line-height: 35px;
+color: #095066;
+grid-column:1/2;
+grid-row:1/2;
+display:flex;
+justify-content:flex-end;
+`;
+const A=styled.a`
+font-size: 16px;
+color: #FFFFFF;
+text-decoration:none;
+`;
+const ButtonsSection=styled.div`
+height:200px;
+grid-column:3/4;
+grid-row:2/3;
+display:flex;
+flex-direction:column;
+justify-content:space-around;
+
+`;
+
+export {ResultSection,Container,YouGot,Button,YourResult,A,ButtonsSection};
